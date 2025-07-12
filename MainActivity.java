016960a547caa51cabee8d7a8de03ba980ceb562
AkashE0706 <eakash0706@gmail.com>
2025-07-12 20:12:01 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DashboardActivity.class);
+            startActivity(intent);
+        });
+    }
+}
